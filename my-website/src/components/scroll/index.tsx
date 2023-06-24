2b00c380b294cf092f60d0704dfedebefd7e4cde
Arslanbek Zhaparov <arslanbekzhaparov@gmail.com>
2023-06-23 18:30:46 -0700

    background decided

--- my-website/src/components/scroll/index.tsx
+++ my-website/src/components/scroll/index.tsx
@@ -1,29 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Background = styled.div<{ showBackground: boolean }>`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background: ${({ showBackground }) =>
-    showBackground ? 'rgba(255, 255, 255, 0.50)' : 'transparent'};
-  box-shadow: ${({ showBackground }) =>
-    showBackground ? '0px 5px 10px 0px rgba(0, 0, 0, 0.25)' : 'none'};
-  backdrop-filter: ${({ showBackground }) =>
-    showBackground ? 'blur(25px)' : 'none'};
   transition: background-color 0.5s, box-shadow 0.5s, backdrop-filter 0.5s;
-  z-index: 1;
+
+  ${({ showBackground }) =>
+    showBackground
+      ? css`
+        background: rgba(0, 0, 0, 0.75);
+        box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.25);
+        backdrop-filter: blur(25px);
+        `
+      : css`
+          background: transparent;
+          box-shadow: none;
+          backdrop-filter: none;
+        `}
+`;
+
+const Text = styled.h1<{ showText: boolean }>`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 24px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: transparent;
+  transition: color 0.5s;
+
+  ${({ showText }) =>
+    showText &&
+    css`
+      color: #ffffff;
+    `}
 `;
 
 const ScrollingBackground: React.FC = () => {
   const [showBackground, setShowBackground] = useState(false);
+  const [showText, setShowText] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       setShowBackground(scrollPosition > 0);
+      setShowText(scrollPosition > 0);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -33,7 +59,11 @@ const ScrollingBackground: React.FC = () => {
     };
   }, []);
 
-  return <Background showBackground={showBackground} />;
+  return (
+    <Background showBackground={showBackground}>
+      <Text showText={showText}>Sample Text</Text>
+    </Background>
+  );
 };
 
 export default ScrollingBackground;
